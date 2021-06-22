d8d3d1edfeafbc930ccdab3f49ef1326f17112c3
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2021-06-22 01:48:55 -0300

    fix(Firebase): Transforming environment variables public to the browser

---
+++ src/pages/auth.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SEO from '@/components/SEO'
+import { Container } from '@/styles/pages/Auth'
+
+export default function Auth(): React.ReactElement {
+  return (
+    <Container>
+      <SEO title="Auth" />
+      Olá
+    </Container>
+  )
+}
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,18 +1,24 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 
 import 'firebase/auth'
 import 'firebase/database'
 
+console.log('=>', process.env.NEXT_PUBLIC_API_KEY)
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 }
-firebase.initializeApp(firebaseConfig)
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+} else {
+  firebase.app() // if already initialized, use that one
+}
 
 export const auth = firebase.auth()
 export const database = firebase.database()
